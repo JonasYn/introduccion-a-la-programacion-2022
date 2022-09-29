9a20c06cb38a0b05b940a66c7b95adc76db9ba23
JonasYn <101908420+JonasYn@users.noreply.github.com>
2022-09-29 00:01:47 -0300

    Update hw3.py

--- hw/hw/hw3/homework-3-JonasYn/hw3.py
+++ hw/hw/hw3/homework-3-JonasYn/hw3.py
@@ -4,7 +4,7 @@
 #
 # Completa con tu nombre, apellido y DNI
 # Nombre y Apellido: jonas ynsaurralde
-# DNI: 44378573
+# DNI: 
 ################################################################################################
 
 
